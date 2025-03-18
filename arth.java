69ad5a39269aeac2ccfbd4e69f5ab6b02288a1c9
abhinav3204l <245122748014@mvsrec.edu.in>
2025-03-18 12:45:55 +0530

    Add files via upload

---
+++ arth.java
@@ -0,0 +1,22 @@
+public class arth {
+    public static void main(String[] args) {
+        // Predefined numbers
+        double num1 = 15;
+        double num2 = 5;
+
+        // Performing arithmetic operations
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num1 / num2;
+
+        // Displaying results
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+    }
+}
+
